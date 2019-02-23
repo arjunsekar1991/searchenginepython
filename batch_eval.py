b4c20272a5eaaf6cbcf48431fd695210518b2273
Arjun <arjunsekar1991@gmail.com>
2019-02-23 01:15:21 -0500

    given files by chen

---
+++ batch_eval.py
@@ -0,0 +1,21 @@
+'''
+a program for evaluating the quality of search algorithms using the vector model
+
+it runs over all queries in query.text and get the top 10 results,
+and then qrels.text is used to compute the NDCG metric
+
+usage:
+    python batch_eval.py index_file query.text qrels.text
+
+    output is the average NDCG over all the queries
+
+'''
+
+def eval():
+
+    # ToDo
+
+    print 'Done'
+
+if __name__ == '__main__':
+    eval()
