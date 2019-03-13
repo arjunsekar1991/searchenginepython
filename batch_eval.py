06a9151706bac1f4787bc84baf060cbd83046a9b
Arjun <arjunsekar1991@gmail.com>
2019-03-13 01:27:57 -0400

    basic working version

--- batch_eval.py
+++ batch_eval.py
@@ -9,12 +9,30 @@ usage:
 
     output is the average NDCG over all the queries
 
+
 '''
 
+from query import query
+from cranqry import loadCranQry
+from index import InvertedIndex
+from cran import CranFile
 def eval():
 
     # ToDo
+    actual = []
+   #
+    qrys = loadCranQry("query.text")
+    #    for q in qrys:
+    #        print(q, qrys[q].text)
 
+    loadiindex = InvertedIndex()
+    loadiindex = loadiindex.load("index_file")
+    #    print("index loaded")
+    cf = CranFile('cran.all')
+    #QueryProcessor.numberofresult =10
+    #qp = QueryProcessor(qrys,loadiindex,cf.docs,10)
+
+    print(query('index_file', '1', 'query.text', '029', 10))
     print('Done')
 
 if __name__ == '__main__':
