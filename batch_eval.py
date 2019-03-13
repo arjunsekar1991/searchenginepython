a6c5e470ab262c9e5fa7ced144c78859f0fa6346
Arjun <arjunsekar1991@gmail.com>
2019-03-13 04:11:13 -0400

    not working 105

--- batch_eval.py
+++ batch_eval.py
@@ -11,19 +11,23 @@ usage:
 
 
 '''
-
+import random
+from functools import reduce
 from query import query
 from cranqry import loadCranQry
 from index import InvertedIndex
 from cran import CranFile
-def eval():
+from metrics import ndcg_score
+import numpy as np
+def eval(numberofrandomqueries):
 
     # ToDo
     actual = []
    #
     qrys = loadCranQry("query.text")
-    #    for q in qrys:
-    #        print(q, qrys[q].text)
+    validqueries = []
+    for q in qrys:
+        validqueries.append(int(q))
 
     loadiindex = InvertedIndex()
     loadiindex = loadiindex.load("index_file")
@@ -31,9 +35,50 @@ def eval():
     cf = CranFile('cran.all')
     #QueryProcessor.numberofresult =10
     #qp = QueryProcessor(qrys,loadiindex,cf.docs,10)
+    queryRelevence = dict()
+    for line in open("qrels.text"):
 
-    print(query('index_file', '1', 'query.text', '029', 10))
-    print('Done')
+        fields = line.split(" ")
+        fields[0] = '%0*d' % (3, int(fields[0]))
+        if fields[0] in queryRelevence:
+            # and let's extract the data:
+            queryRelevence[fields[0]].append(fields[1])
+        else:
+            # create a new array in this slot
+            queryRelevence[fields[0]] = [fields[1]]
+
+
+    relevent = list(queryRelevence.keys())
+    relevent = list(map(int, relevent))
+    samplespace = np.intersect1d(relevent, validqueries)
+    list_of_random_items = random.sample(list(samplespace), numberofrandomqueries)
+    tempcounter2 = 0
+
+    while tempcounter2<numberofrandomqueries:
+
+        list_of_random_items[tempcounter2] = '%0*d' % (3, int(list_of_random_items[tempcounter2]))
+        print('query for which ndcg is calculated '+ str(list_of_random_items[tempcounter2]))
+        y = str(list_of_random_items[tempcounter2])
+        vectorresult = query('index_file', '1', 'query.text', '105', 10)
+        tempcounter =0
+        for k in vectorresult:
+
+            if k in queryRelevence[str(list_of_random_items[tempcounter2])]:
+                vectorresult[tempcounter]=1
+            else:
+                vectorresult[tempcounter]=0
+            tempcounter = tempcounter + 1
+        #print(vectorresult)
+        idealvectorresult = vectorresult.copy()
+        idealvectorresult.sort(reverse=True)
+        #print(idealvectorresult)
+        if sum(idealvectorresult) == 0:
+            ndcgscore = 0
+        else:
+            ndcgscore = ndcg_score(idealvectorresult,vectorresult)
+        print(ndcgscore)
+        print('Done')
+        tempcounter2 = tempcounter2 - 1
 
 if __name__ == '__main__':
-    eval()
+    eval(3)
